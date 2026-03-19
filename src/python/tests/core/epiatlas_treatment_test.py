c23ba5d7d8b06fdbb83f0c4130f615c5b2f0bdf7
rabyj <Joanny.Raby@usherbrooke.ca>
2026-03-19 14:57:12 -0400

    Remove old one-time sanity check test

--- src/python/tests/core/epiatlas_treatment_test.py
+++ src/python/tests/core/epiatlas_treatment_test.py
@@ -237,33 +237,3 @@ class TestEpiAtlasFoldFactory:
             assert 0.8 <= ratio <= 1.2
 
 
-@pytest.mark.skip(reason="One time. For sklearn code sanity check.")
-def test_StratifiedKFold_sanity():
-    """Test that StratifiedKFold yields same datasets every time.
-
-    Preconditions: Exact same input labels list. (raw_dset.train.encoded_labels)
-    """
-    skf1 = StratifiedKFold(n_splits=5, shuffle=False)
-    skf2 = StratifiedKFold(n_splits=5, shuffle=False)
-    n_classes = 10
-    num_examples = 150
-    labels = np.random.choice(n_classes, size=num_examples)
-
-    run_1 = list(
-        skf1.split(
-            np.zeros((num_examples, n_classes)),
-            list(labels),
-        )
-    )
-    run_2 = list(
-        skf2.split(
-            np.zeros((num_examples, n_classes)),
-            list(labels),
-        )
-    )
-
-    for elem1, elem2 in zip(run_1, run_2):
-        train1, valid1 = elem1
-        train2, valid2 = elem2
-        assert np.array_equal(train1, train2)
-        assert np.array_equal(valid1, valid2)
